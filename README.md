2656b2dce6a977538126b8461c4da847732eabd4
Owen Dando <owen.dando@ed.ac.uk>
2015-08-26 14:16:31 +0100

    Add package skeleton.

---
+++ README.md
@@ -0,0 +1,4 @@
+Species Separator
+=================
+
+TODO.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+import species_separator
+
+setup(
+    name="TODO",
+    version=species_separator.__version__,
+    url='https://github.com/lweasel/species-separator',
+    license='MIT License',
+    author='TODO',
+    author_email='TODO',
+    packages=['species_separator'],
+    install_requires=[
+    ],
+    scripts=[
+    ]
+)
