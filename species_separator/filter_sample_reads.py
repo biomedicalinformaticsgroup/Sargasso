1ac5f5213e16770271980d74e4089d1d6d1d15aa
Owen Dando <owen.dando@ed.ac.uk>
2015-09-10 17:33:41 +0100

    Speed up by lazy evaluation of hit info.

--- species_separator/filter_sample_reads.py
+++ species_separator/filter_sample_reads.py
@@ -90,37 +90,40 @@ def _validate_command_line_options(options):
 
 class _HitsInfo:
     def __init__(self, hits):
+        self.hits = hits
         self.multimaps = None
-        self.max_mismatches = 0
-        self.min_mismatches = sys.maxsize
-        self.cigar = {}
-
-        for hit in hits:
-            if self.multimaps is None:
-                self.multimaps = samutils.get_multimaps(hit)
-            elif self.multimaps != samutils.get_multimaps(hit):
-                # TODO: report error - multimaps value should be the same for
-                # all hits
-                raise(Exception)
-
-            mismatches = samutils.get_mismatches(hit)
-            if mismatches > self.max_mismatches:
-                self.max_mismatches = mismatches
-            if mismatches < self.min_mismatches:
-                self.min_mismatches = mismatches
-
-            self.cigar[hit] = samutils.get_cigar(hit)
+        self.max_mismatches = None
+        self.min_mismatches = None
+        self.cigar = None
 
     def get_multimaps(self):
+        if self.multimaps is None:
+            self.multimaps = samutils.get_multimaps(self.hits[0])
         return self.multimaps
 
     def get_max_mismatches(self):
+        if self.max_mismatches is None:
+            self.max_mismatches = 0
+            for hit in self.hits:
+                mismatches = samutils.get_mismatches(hit)
+                if mismatches > self.max_mismatches:
+                    self.max_mismatches = mismatches
         return self.max_mismatches
 
     def get_min_mismatches(self):
+        if self.min_mismatches is None:
+            self.min_mismatches = sys.maxsize
+            for hit in self.hits:
+                mismatches = samutils.get_mismatches(hit)
+                if mismatches < self.min_mismatches:
+                    self.min_mismatches = mismatches
         return self.min_mismatches
 
     def get_cigars(self):
+        if self.cigar is None:
+            self.cigar = {}
+            for hit in self.hits:
+                self.cigar[hit] = samutils.get_cigar(hit)
         return self.cigar.values()
 
 
