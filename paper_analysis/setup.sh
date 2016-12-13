e880d42fe1faf1f2660d0eb5de7d2a66e7159124
Owen Dando <owen.dando@ed.ac.uk>
2016-12-13 15:37:22 +0000

    Temporarily remove paper analysis scripts.

--- paper_analysis/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pip install git+https://github.com/lweasel/transcript_utils.git@6555f98618ab7a369ecb489e5ccf7676547faed2
-
-#TODO - download genome data
-#TODO - download gene and ortholog info from Ensembl
-#TODO - download RNA-seq data when it's publically available
-#TODO - build STAR indices
